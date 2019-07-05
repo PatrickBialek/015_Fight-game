0b8ae7e2188e442f3158121a5d966c0f4c01d6e9
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-07-06 00:46:09 +0200

    Working on first functions

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,48 @@
 <template>
-  <div class="game container"></div>
+  <div class="game container">
+    <div class="columns">
+      <div class="column is-6">
+        <h2>Player:</h2>
+        <div class="game__live-bar-container">
+          <div class="game__live-bar"></div>
+        </div>
+      </div>
+      <div class="column is-6">
+        <h2>Enemy:</h2>
+        <div class="game__live-bar-container">
+          <div class="game__live-bar"></div>
+        </div>
+      </div>
+    </div>
+    <div v-if="!gameIsRunning" class="columns is-centered">
+      <div class="column has-text-centered">
+        <button @click.prevent="gameRuningHandler" class="button is-info">Start</button>
+      </div>
+    </div>
+    <div v-else class="columns is-centered">
+      <div class="column is-5 has-text-centered">
+        <button class="button is-warning">Sword</button>
+        <button class="button is-danger">Axe</button>
+        <button class="button is-info">Heal</button>
+        <button @click.prevent="gameRuningHandler" class="button is-dark">Give up</button>
+      </div>
+    </div>
+    <div class="columns"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "Main",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      gameIsRunning: false
     };
+  },
+  methods: {
+    gameRuningHandler() {
+      this.gameIsRunning = !this.gameIsRunning;
+    }
   }
 };
 </script>
@@ -18,4 +52,5 @@ export default {
 
   .game
     height: calc(100vh - 102px)
+    padding: 50px 0
 </style>
